9bca7481f63f10e0e5adf07fbb4eda0ef4bada18
Henrique194 <henriquejb194@gmail.com>
2021-07-03 10:41:59 -0300

    PNG-Framework

--- Collections/src/main/Main.java
+++ Collections/src/main/Main.java
@@ -1,5 +1,14 @@
 package main;
 
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Hashtable;
+
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
